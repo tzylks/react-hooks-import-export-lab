f7c9ea74f1b346a10c8cbd39a0ed459c57e6789e
TrevorZylks <trevorzylks@gmail.com>
2021-07-20 09:53:21 -0700

    commit tree 35152028775e32be80db68b954e73e9d46034e86

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import * as data from "../data/user.js"
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {data.username} is a Web Developer from {data.city}
       </h1>
     </div>
   );
+  
 }
+
+export default Home
