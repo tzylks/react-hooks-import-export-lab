3e82d657ddae06538526b16a48439086c8e49527
TrevorZylks <trevorzylks@gmail.com>
2021-07-20 09:53:30 -0700

    Complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,14 @@
 import React from "react";
+import * as data from "../data/user.js"
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={data.image} alt="I made this" />
     </div>
   );
 }
+
+export default About
